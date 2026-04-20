e2739908b2b5409bd69caabe2358f286e97e94c4
Alex Barra <alex.barra@berkeley.edu>
2026-04-20 13:16:29 -0700

    Fix formatting and remove unnecessary characters

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://svnkjucrjpnjdezbodtc.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2bmtqdWNyanBuamRlemJvZHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMDIyMjQsImV4cCI6MjA3OTc3ODIyNH0.nWQTeRlOER_xJEoAE2TxA0mJkMcLKAIyFxkj9xJJ1xE
-Y';
+';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
